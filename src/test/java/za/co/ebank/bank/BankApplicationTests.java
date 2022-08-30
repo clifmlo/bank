505bea685c1d9541c4da1c4248fc6f22bed15c51
Cliford <cliford.mlotshwa@gmail.com>
2022-08-30 22:36:04 +0200

    initial commit

---
+++ src/test/java/za/co/ebank/bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.ebank.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
